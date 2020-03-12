a70f789cf4a2de651170ee518c7cfcb1a0324839
Matjaz Zorko <matjaz@apto.si>
2020-03-12 13:46:11 +0100

    Product item & filter logic

--- src/scripts/components/collection.js
+++ src/scripts/components/collection.js
@@ -1,40 +1,307 @@
 import $ from "jquery";
-import "slick-carousel";
 
-const elements = {
-  slider: "[data-collection-slider]",
+const dataSets = {
+  catOptionBtn: "cat-option",
+  catOptionWrap: "cat-sub-wrap",
+  piColorValue: "pi-option-value",
+  piSizeValue: "pi-option-size",
+  piMouthValue: "pi-option-mouth",
+  subList: "cat-sub-list",
+  subBtn: "cat-sub-button",
+  currentFilter: "cat-current-item",
+  currentType: "cat-current-type"
 };
 
-function slider() {
-  const $slider = $(elements.slider);
-  if ($slider.length > 0 && $slider.find(">div").length > 1) {
-    $slider.slick({
-      swipeToSlide: true,
-      arrows: true,
-      dots: true,
-      slidesToShow: 3,
-      nextArrow: `<button type='button' class='slick-next slick-arrow'><span class='icon-fallback-text'>
-        ${theme.strings.next}
-        </span></button>`,
-      prevArrow: `<button type='button' class='slick-prev slick-arrow'><span class='icon-fallback-text'>
-        ${theme.strings.previous}
-      </span></button>`,
-      responsive: [
-        {
-          breakpoint: 992,
-          settings: {
-            slidesToShow: 2,
-          },
-        },
-        {
-          breakpoint: 768,
-          settings: {
-            slidesToShow: 1,
-          },
-        },
-      ],
-    });
-  }
+const selectors = {
+  collection: "[data-collection]",
+  product: "[data-pi-item]",
+  productCol: "[data-pi-item-col]",
+  main: "[data-cat-button]",
+  list: "[data-cat-list]",
+  subBtn: `[data-${dataSets.subBtn}]`,
+  subList: `[data-${dataSets.subList}]`,
+  catOptionBtn: `[data-${dataSets.catOptionBtn}]`,
+  catOptionWrap: `[data-${dataSets.catOptionWrap}]`,
+  currentParent: "[data-cat-current]",
+  currentFilter: `[data-${dataSets.currentFilter}]`,
+  currentType: `[data-${dataSets.currentType}]`,
+  i: {
+    close: "[data-icon-close-rendered]"
+  },
+  filterReset: "[data-cat-filter-reset]"
+};
+
+const classNames = {
+  active: "active"
+};
+
+const strings = {
+  size: "Size",
+  mouth: "Mouth",
+  color: "Color",
+  noFilter: "No filter"
+};
+
+const timers = {
+  currentF: 200,
+  product: 600,
+  filterL: 200,
+  filterDefault: 250,
+  resetDefault: 250
+};
+
+function handleCatButtonClick(event) {
+  event.preventDefault();
+
+  const $source = $(event.currentTarget);
+  $source.toggleClass(classNames.active);
+  $(document)
+    .find(selectors.list)
+    .slideToggle(timers.filterL);
+
+  $(document)
+    .find(selectors.subList)
+    .slideUp(timers.filterL);
+
+  $(document)
+    .find(selectors.subBtn)
+    .removeClass(classNames.active);
+
+  return null;
 }
 
-$(document).ready(slider);
+function handleCatSubButtonClick(event) {
+  event.preventDefault();
+  const $source = $(event.currentTarget);
+
+  const type = $source.data(dataSets.subBtn);
+  const $list = $(document).find(`[data-${dataSets.subList}="${type}"]`);
+
+  $(document)
+    .find(selectors.subList)
+    .not($list)
+    .slideUp(timers.filterL);
+
+  $(document)
+    .find(selectors.subBtn)
+    .not($source)
+    .removeClass(classNames.active);
+
+  $source.toggleClass(classNames.active);
+  $list.slideToggle(timers.filterL);
+  return null;
+}
+
+function handleCatOptionClick(event) {
+  event.preventDefault();
+  const $source = $(event.currentTarget);
+
+  handleFilterChange($source);
+
+  return filterLogic(timers.currentF + 50);
+}
+
+function handleFilterChange($source) {
+  if (!$source || $source.length === 0) {
+    return null;
+  }
+
+  const value = $source.data(dataSets.catOptionBtn);
+  const $parent = $source.closest(selectors.subList);
+  const type = $parent.data(dataSets.subList);
+
+  if (type === strings.color) {
+    handleProductColorSelect(value);
+  }
+
+  return handleCurrentFilterBtn(value, type);
+}
+
+function handleProductColorSelect(value) {
+  const $products = $(selectors.product);
+  $products.find(`[data-${dataSets.piColorValue}="${value}"]`).click();
+}
+
+let filterTimerHolder;
+function filterLogic(timer) {
+  clearTimeout(filterTimerHolder);
+  filterTimerHolder = setTimeout(
+    () => {
+      let activeCount = 0;
+      const $pItems = $(selectors.product);
+      for (let i = 0; i < $pItems.length; i++) {
+        const $pItem = $($pItems[i]);
+
+        let hasColor = true;
+        let hasMouth = true;
+        let hasSize = true;
+
+        const mouthValue = getCurrentFilterValueByType(strings.mouth);
+        const colorValue = getCurrentFilterValueByType(strings.color);
+        const sizeValue = getCurrentFilterValueByType(strings.size);
+
+        hasSize = checkIfProductHasSize($pItem, sizeValue);
+        hasMouth = checkIfProductHasMouth($pItem, mouthValue);
+        hasColor = checkIfProductHasColor($pItem, colorValue);
+
+        if (hasColor && hasMouth && hasSize) {
+          $pItem.closest(selectors.productCol).slideDown(timers.product);
+          activeCount++;
+        } else {
+          $pItem.closest(selectors.productCol).slideUp(timers.product);
+        }
+      }
+
+      handleActiveProductCount(activeCount);
+      return checkIfActiveFilters(10);
+    },
+    timer ? timer : timers.filterDefault
+  );
+}
+
+function getCurrentFilterValueByType(type) {
+  const $currentParent = $(selectors.currentParent);
+  const $currentSameType = $currentParent.find(
+    `[data-${dataSets.currentType}="${type}"]`
+  );
+
+  if ($currentSameType.length === 0) {
+    return strings.noFilter;
+  }
+
+  const value = $currentSameType.data(dataSets.currentFilter);
+  return value;
+}
+
+function checkIfProductHasColor($item, value) {
+  if (value === strings.noFilter) {
+    return true;
+  }
+
+  const $requiredOption = $item.find(
+    `[data-${dataSets.piColorValue}="${value}"]`
+  );
+
+  if ($requiredOption.length && $requiredOption.length > 0) {
+    return true;
+  }
+
+  return false;
+}
+
+function checkIfProductHasSize($item, value) {
+  if (value === strings.noFilter) {
+    return true;
+  }
+
+  const $requiredOption = $item.find(
+    `[data-${dataSets.piSizeValue}="${value}"]`
+  );
+
+  if ($requiredOption.length && $requiredOption.length > 0) {
+    return true;
+  }
+
+  return false;
+}
+
+function checkIfProductHasMouth($item, value) {
+  if (value === strings.noFilter) {
+    return true;
+  }
+
+  const $requiredOption = $item.find(
+    `[data-${dataSets.piMouthValue}="${value}"]`
+  );
+
+  if ($requiredOption.length && $requiredOption.length > 0) {
+    return true;
+  }
+
+  return false;
+}
+
+function handleActiveProductCount(count) {
+  if (count === 0) {
+    // console.log("No prodcuts. Please adjust filters.");
+  }
+
+  return null;
+}
+
+function resetCurrentFilters() {
+  const $currentParent = $(selectors.currentParent);
+  $currentParent.children().fadeOut(timers.currentF, () => {
+    $currentParent.children().remove();
+    filterLogic(20);
+  });
+}
+
+function removeCurrentFilterOnClick(event) {
+  const $source = $(event.currentTarget);
+  $source.fadeOut(timers.currentF, () => {
+    $source.remove();
+    filterLogic(20);
+  });
+}
+
+function handleCurrentFilterBtn(value, type) {
+  const $currentParent = $(selectors.currentParent);
+  const $currentSameType = $currentParent.find(
+    `[data-${dataSets.currentType}="${type}"]`
+  );
+
+  const $newButton = $(getCurrentButtonPattern(value, type));
+  $newButton.hide();
+  $currentParent.append($newButton);
+
+  if ($currentSameType.length > 0) {
+    $currentSameType.fadeOut(timers.currentF, () => {
+      $currentSameType.remove();
+      $newButton.fadeIn(timers.currentF);
+    });
+  } else {
+    $newButton.fadeIn(timers.currentF);
+  }
+
+  return null;
+}
+
+let resetCheckHolder;
+function checkIfActiveFilters(time) {
+  clearTimeout(resetCheckHolder);
+  resetCheckHolder = setTimeout(
+    () => {
+      const $filters = $(selectors.currentFilter);
+      const $reset = $(selectors.filterReset);
+      if ($filters.length > 0) {
+        $reset.slideDown(timers.currentF);
+      } else {
+        $reset.slideUp(timers.currentF);
+      }
+    },
+    time ? time : timers.resetDefault
+  );
+}
+
+function getCurrentButtonPattern(value, type) {
+  if (!value) {
+    return "";
+  }
+
+  const $closeIconElement = $(selectors.i.close);
+  const icon = $closeIconElement.length > 0 ? $closeIconElement.html() : "";
+  const valueClean = value.replace("size-", "").replace("mouth-", "");
+
+  const pattern = `<button type="button" class="c-filter__current-filters" data-cat-current-item="${value}" data-cat-current-type="${type}">
+    <span class="c-filter__current-title">${valueClean}</span>
+    <span class="c-filter__current-icon">${icon}</span>
+  </button>`;
+  return pattern;
+}
+
+$(document).on("click", selectors.main, handleCatButtonClick);
+$(document).on("click", selectors.subBtn, handleCatSubButtonClick);
+$(document).on("click", selectors.catOptionBtn, handleCatOptionClick);
+$(document).on("click", selectors.filterReset, resetCurrentFilters);
+$(document).on("click", selectors.currentFilter, removeCurrentFilterOnClick);
---
+++ src/scripts/components/product-item.js
@@ -0,0 +1,212 @@
+import $ from "jquery";
+import { formatMoney } from "@shopify/theme-currency";
+
+const classes = {
+  active: "active"
+};
+
+const datasets = {
+  value: "pi-option-value",
+  image: "pi-image",
+  gallery: "pi-image-gallery",
+  colors: "pi-color-options",
+  opName: "pi-option-name",
+  option: "pi-option",
+  index: "pi-option-index"
+};
+
+const selectors = {
+  value: `[data-${datasets.value}]`,
+  image: `[data-${datasets.image}]`,
+  gallery: `[data-${datasets.gallery}]`,
+  current: `.${classes.active}[data-${datasets.image}]`,
+  colors: `[data-${datasets.colors}]`,
+  item: "[data-pi-item]",
+  opName: `[data-${datasets.opName}]`,
+  json: "[data-product-json]",
+  select: "[data-pi-variant-select]",
+  option: `[data-${datasets.option}]`,
+  submit: "[data-pi-submit]",
+  submitprice: "[data-pi-submit-price]",
+  submitText: "[data-pi-submit-text]",
+  price: "[data-pi-price]",
+  compare: "[data-pi-compare]",
+  index: `[data-${datasets.index}]`
+};
+
+function init() {
+  offsetColorSwatches();
+}
+
+function handleOptionClick(event) {
+  const $source = $(event.currentTarget);
+  const opName = $source.data(datasets.opName);
+
+  if ($source.length === 0 || !opName) {
+    return false;
+  }
+
+  if (opName === "color") {
+    handleColorChange($source);
+  }
+
+  return handleVariantChange($source);
+}
+
+function handleColorChange($source) {
+  const color = $source.data(datasets.value);
+  const $parent = $source.closest(selectors.item);
+  const $newImage = $parent.find(`[data-${datasets.image}="${color}"]`);
+  const $currentImage = $parent.find(selectors.current);
+
+  if (!$newImage.hasClass(classes.active)) {
+    $currentImage.fadeOut("fast", () => {
+      $newImage.fadeIn("fast");
+      $newImage.addClass(classes.active);
+      $currentImage.removeClass(classes.active);
+    });
+  }
+}
+
+function handleVariantChange($source) {
+  const $parent = $source.closest(selectors.item);
+  const $jsonElement = $parent.find(selectors.json);
+  const $select = $parent.find(selectors.select);
+
+  if (
+    $parent.length === 0 ||
+    $jsonElement.length === 0 ||
+    $select.length === 0
+  ) {
+    return false;
+  }
+
+  const variants = JSON.parse($jsonElement.html());
+  const $options = $parent.find(selectors.option);
+
+  const newVariant = getSelectedVariant(variants.variants, $options, $source);
+
+  return renderVariant(newVariant, $parent);
+}
+
+function getSelectedVariant(variants, $options, $new) {
+  //  exclude $new to only get the current variant
+  const optionValues = [];
+  $options.each((index, option) => {
+    const $input = $(option).find("input:checked");
+    optionValues.push($input.val());
+  });
+
+  const options = {
+    Option1: optionValues[0] || null,
+    Option2: optionValues[1] || null,
+    Option3: optionValues[2] || null
+  };
+
+  if ($new && $new.length > 0) {
+    const index = $new.data(datasets.index);
+    const value = $new.data(datasets.value);
+    const indexName = `Option${index}`;
+    options[indexName] = value;
+  }
+
+  for (let i = 0; i < variants.length; i++) {
+    const variant = variants[i];
+    if (
+      variant.Option1 === options.Option1 &&
+      variant.Option2 === options.Option2 &&
+      variant.Option3 === options.Option3
+    ) {
+      return variant;
+    }
+  }
+
+  return null;
+}
+
+function renderVariant(variant, $parent) {
+  if (!$parent || $parent.length === 0) {
+    return null;
+  }
+
+  renderProductOptions(variant, $parent);
+  renderProductPrice(variant, $parent);
+  renderProductSubmit(variant, $parent);
+  return null;
+}
+
+function renderProductPrice(variant, $parent) {
+  if (!variant || variant.length === 0) {
+    return null;
+  }
+  const price = variant.price;
+  const compareAt = variant.compare_at;
+
+  const $regularPriceElement = $parent.find(selectors.price);
+  const $comparePriceElement = $parent.find(selectors.compare);
+  const $submitPriceElement = $parent.find(selectors.submitprice);
+
+  $regularPriceElement.html(
+    formatMoney(price, theme.moneyFormat).replace(".00", "")
+  );
+  $submitPriceElement.html(
+    `${formatMoney(price, theme.moneyFormat).replace(".00", "")} `
+  );
+  if (compareAt && compareAt > 0) {
+    $comparePriceElement.html(
+      formatMoney(compareAt, theme.moneyFormat).replace(".00", "")
+    );
+  } else {
+    $comparePriceElement.html("");
+  }
+  return null;
+}
+
+function renderProductSubmit(variant, $parent) {
+  const $submit = $parent.find(selectors.submit);
+  const $submitText = $parent.find(selectors.submitText);
+
+  if (!variant) {
+    $submit.attr("disabled", "disabled");
+    $submitText.text(theme.strings.unavailable);
+  } else if (variant.available) {
+    $submit.removeAttr("disabled");
+    $submitText.text(theme.strings.addToCart);
+  } else {
+    $submit.attr("disabled", "disabled");
+    $submitText.text(theme.strings.soldOut);
+  }
+  return null;
+}
+
+function renderProductOptions(variant, $parent) {
+  if (!$parent || $parent.length === 0 || !variant) {
+    return null;
+  }
+  const $select = $parent.find(selectors.select);
+  const $newOption = $select.find(`option[value="${variant.id}"]`);
+  $newOption.attr("selected", "selected");
+  return $select.change();
+}
+
+function offsetColorSwatches() {
+  const $allColors = $(selectors.colors);
+  $allColors.each((index, value) => {
+    const $colors = $(value);
+    const $product = $colors.closest(selectors.item);
+    const $gallery = $product.find(selectors.gallery);
+    const height = $gallery.height();
+    // 22 = margin:10px, height: 12px
+    $colors.css("top", height - 22);
+  });
+}
+
+function handleWidthTransition() {
+  offsetColorSwatches();
+}
+
+$(document).on("click", selectors.value, handleOptionClick);
+
+$(document).on("windowWidthChanged", handleWidthTransition);
+
+$(document).ready(init);
