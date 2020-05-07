da2aa652352fa7ba10fb92804d1db09eedf9e11c
Matjaz Zorko <matjaz@apto.si>
2020-05-07 14:21:33 +0200

    Feature/blog listing

---
+++ src/scripts/components/blog-newsletter-modal.js
@@ -0,0 +1,51 @@
+import $ from "jquery";
+
+const elements = {
+  newsletterBtn: "[data-newsletter-open-btn]",
+  newsletterModal: "[data-newsletter-modal]",
+  newsletterModalOverlay: "[data-newsletter-modal-overlay]",
+  openModalSelector: "modal-opened"
+};
+
+const $newsletterModal = $(elements.newsletterModal);
+const $newsletterModalOverlay = $(elements.newsletterModalOverlay);
+
+function init() {
+  const $newsletterOpenBtn = $(elements.newsletterBtn);
+  if ($newsletterModal.hasClass("only--mobile") && $(window).width() > 768) {
+    destroy();
+    return false;
+  }
+
+  $newsletterOpenBtn.on("click", openModal);
+
+  $(document).on("click", closeModal);
+
+  $newsletterModal.on("click", (event) => {
+    event.stopPropagation();
+    return false;
+  });
+
+  return true;
+}
+
+function openModal() {
+  $newsletterModal.addClass(elements.openModalSelector);
+  $newsletterModalOverlay.addClass(elements.openModalSelector);
+
+  return false;
+}
+
+function closeModal() {
+  $newsletterModal.removeClass(elements.openModalSelector);
+  $newsletterModalOverlay.removeClass(elements.openModalSelector);
+}
+
+function destroy() {
+  $newsletterModal.removeClass(elements.openModalSelector);
+  $newsletterModalOverlay.removeClass(elements.openModalSelector);
+  $newsletterModal.off("click");
+}
+
+$(document).ready(init);
+
