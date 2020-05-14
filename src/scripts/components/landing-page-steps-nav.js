89a33ff37b10516eac6f9badef0f6cd29750242b
Jernej Droljc <droljc.jernej@gmail.com>
2020-05-14 22:04:41 +0200

    Add steps navigation JS for section steps. Add styles for steps.

---
+++ src/scripts/components/landing-page-steps-nav.js
@@ -0,0 +1,55 @@
+import $ from "jquery";
+
+const elements = {
+  stepSelector: "[data-step]",
+  navSelect: "[data-nav]",
+  prevStepBtn: "[data-prev-step]",
+  nextStepBtn: "[data-next-step]",
+};
+
+const classes = {
+  activeStep: "active-step",
+  inactiveStep: "inactive-step"
+};
+
+const $prevStepBtn = $(elements.prevStepBtn);
+const $nextStepBtn = $(elements.nextStepBtn);
+const $navStep = $(elements.stepSelector);
+let stepCount = 0;
+
+function init() {
+  $nextStepBtn.on("click", nextStep);
+  $prevStepBtn.on("click", prevStep);
+
+  return true;
+}
+
+function nextStep() {
+
+  $navStep.eq(stepCount).fadeOut();
+
+  stepCount++;
+  $navStep.eq(stepCount).css("display", "flex")
+    .hide()
+    .fadeIn();
+
+
+  return false;
+}
+
+function prevStep() {
+  $navStep.eq(stepCount).fadeOut();
+
+  stepCount--;
+  $navStep.eq(stepCount).css("display", "flex")
+    .hide()
+    .fadeIn();
+
+
+  return false;
+}
+
+if ($(window).width() <= 768) {
+  $(document).ready(init);
+}
+
--- src/scripts/templates/page.landing-page.js
+++ src/scripts/templates/page.landing-page.js
@@ -1,7 +1,7 @@
 import { load } from "@shopify/theme-sections";
 
 import "../components/landing-page-modal";
-// import "../components/landing-page-steps-nav";
+import "../components/landing-page-steps-nav";
 
 import "../../styles/page.landing-page.scss";
 
