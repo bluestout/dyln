ae4d6acd8e1c9e553066afd5cbd157321b6bf0f6
Matjaz Zorko <matjaz@apto.si>
2020-05-22 18:58:50 +0200

    Separate subscription form and pdp-diffusers

---
+++ src/scripts/components/pdp-subscription.js
@@ -0,0 +1,14 @@
+import $ from "jquery";
+
+const classes = {
+  hide: "hide",
+  active: "active",
+};
+
+const datasets = {
+  atc: {
+    option: "atc-option",
+  },
+};
+
+const selectors = {};
--- src/scripts/templates/product.js
+++ src/scripts/templates/product.js
@@ -3,6 +3,7 @@ import { load } from "@shopify/theme-sections";
 import "../components/custom-qty-input";
 import "../components/tabs";
 import "../sections/product";
+import "../components/pdp-subscription";
 
 import "../../styles/product.scss";
 
