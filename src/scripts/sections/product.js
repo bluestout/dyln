e2ca62e0a5633eeb2efb3c232cdff1e5233f8350
Jernej Droljc <37811988+jernejdroljc@users.noreply.github.com>
2020-06-17 17:23:43 +0200

    Add general fixes for product detail page, contact form pages and footer. (#33)

--- src/scripts/sections/product.js
+++ src/scripts/sections/product.js
@@ -141,6 +141,9 @@ register("product", {
   onFormOptionChange(event) {
     const variant = event.dataset.variant;
 
+    $(".pdp-form__color-input").removeClass("active-variant");
+    $(event.target).addClass("active-variant");
+
     // this.renderImages(variant);
     this.renderPrice(variant);
     this.renderComparePrice(variant);
@@ -577,6 +580,7 @@ function handleAtcOptionChange(optionName, toggle) {
 function handleAtcOptionClick(event) {
   const $source = $(event.currentTarget);
   const targetData = $source.data(datasets.atc.option);
+
   handleAtcOptionChange(targetData, true);
 }
 
