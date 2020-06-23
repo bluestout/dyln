e1407e4e41cac1c66e6f1be1a2259e43743a26c7
Jernej Droljc <droljc.jernej@gmail.com>
2020-06-23 14:57:06 +0200

    Add video section back button.

--- src/scripts/components/index.js
+++ src/scripts/components/index.js
@@ -2,6 +2,7 @@ import $ from "jquery";
 
 const selectors = {
   button: "[data-index-video-open]",
+  backBtn: "[data-index-video-back-btn]",
   videoWrap: "[data-index-video]",
   contentWrap: "[data-index-video-content]",
   bg: "[data-index-video-bg]",
@@ -22,4 +23,17 @@ function videoOpen() {
   });
 }
 
+function sectionOpen() {
+  const $content = $(selectors.contentWrap);
+  const $wrap = $(selectors.videoWrap);
+  const $bg = $(selectors.bg);
+
+  $bg.toggleClass(classes.active);
+  $wrap.fadeOut("fast", () => {
+    $content.fadeIn();
+  });
+
+}
+
 $(document).on("click", selectors.button, videoOpen);
+$(document).on("click", selectors.backBtn, sectionOpen);
