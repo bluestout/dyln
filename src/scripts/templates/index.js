c3a66072e24ea436eb540af4eef0a9390c227ce4
Matjaz Zorko <matjaz@apto.si>
2020-07-23 19:23:59 +0200

    fix custom slider

--- src/scripts/templates/index.js
+++ src/scripts/templates/index.js
@@ -3,8 +3,6 @@ import { load } from "@shopify/theme-sections";
 import "../components/index";
 import "../components/user-stories";
 import "../components/product-item";
-import "../components/jquery.mobile-events";
-import "../components/custom-slider";
 
 import "../../styles/index.scss";
 
