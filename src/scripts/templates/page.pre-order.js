35edd266269a165a9b6a6423997c57c00683d2e8
Jernej Droljc <droljc.jernej@gmail.com>
2020-07-27 21:22:45 +0200

    Add video section JS & styles.

--- src/scripts/templates/page.pre-order.js
+++ src/scripts/templates/page.pre-order.js
@@ -1,6 +1,7 @@
 import { load } from "@shopify/theme-sections";
 
 import "../components/pre-order-testimonials-slider";
+import "../components/pre-order-video";
 
 import "../../styles/page.pre-order.scss";
 
