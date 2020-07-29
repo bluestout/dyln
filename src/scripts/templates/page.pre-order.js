ea2f7630ba51ab0f8c8fb8e00494533333ccdce5
Jernej Droljc <droljc.jernej@gmail.com>
2020-07-29 15:53:42 +0200

    Add tablet styles modifications.

--- src/scripts/templates/page.pre-order.js
+++ src/scripts/templates/page.pre-order.js
@@ -3,6 +3,7 @@ import { load } from "@shopify/theme-sections";
 import "../components/pre-order-testimonials-slider";
 import "../components/pre-order-video";
 import "../components/product-item";
+import "../components/pre-order-header";
 
 import "../../styles/page.pre-order.scss";
 
