6adf7648162ea07020dffc45088dab37b78df966
Matjaz Zorko <matjaz@apto.si>
2020-08-12 19:04:05 +0200

    Fix the blog files compile method

--- src/scripts/templates/blog.tagged.js
+++ src/scripts/templates/blog.tagged.js
@@ -4,6 +4,6 @@ import "../components/blog-vlog";
 import "../components/blog-search";
 import "../components/blog-newsletter-modal";
 
-import "../../styles/blog.scss";
+import "../../styles/blog.tagged.scss";
 
 load("*");
